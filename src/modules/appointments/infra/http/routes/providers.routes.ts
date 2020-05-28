078a7516e0c1d0eec56507de5202b54cad4bc7d2
Harca <lucas.harca@gmail.com>
2020-05-28 10:05:45 -0300

    added providers availability index

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import MonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import DayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const monthAvailabilityController = new MonthAvailabilityController();
+const dayAvailabilityController = new DayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  monthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  dayAvailabilityController.index,
+);
 
 export default providersRouter;
